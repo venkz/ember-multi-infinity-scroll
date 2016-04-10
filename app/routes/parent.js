727c34e69f9de34b2c0efb2450808bf25a44068c
Venkatesh Kara <vkara@VKARA_MBP.nimblestorage.com>
2016-04-10 00:46:21 -0700

    controller's target is deepest route behavior confirmation - Buttons test

--- app/routes/parent.js
+++ app/routes/parent.js
@@ -4,5 +4,11 @@ import InfinityRoute from "ember-infinity/mixins/route";
 export default Ember.Route.extend(InfinityRoute, {
   model() {
     return this.infinityModel("contact", { perPage: 50, startingPage: 1 });
+  },
+
+  actions: {
+    commonRouteAction() {
+      console.log("This is PARENT reporting");
+    }
   }
 });
--- app/routes/parent/child.js
+++ app/routes/parent/child.js
@@ -4,5 +4,11 @@ import InfinityRoute from "ember-infinity/mixins/route";
 export default Ember.Route.extend(InfinityRoute, {
   model() {
     return this.infinityModel("user", { perPage: 50, startingPage: 1 });
+  },
+
+  actions: {
+    commonRouteAction() {
+      console.log("This is CHILD reporting");
+    }
   }
 });
