cfba20b78316266ea170136e8fd11a7813cc7b65
Hugh Francis <me@hughfrancis.me>
2016-04-11 11:32:39 -0400

    Namespace Parent Action

--- app/routes/parent.js
+++ app/routes/parent.js
@@ -4,5 +4,8 @@ import InfinityRoute from "ember-infinity/mixins/route";
 export default Ember.Route.extend(InfinityRoute, {
   model() {
     return this.infinityModel("contact", { perPage: 50, startingPage: 1 });
+  },
+  actions: {
+    parentInfinityLoad() { this._infinityLoad(); } 
   }
 });
